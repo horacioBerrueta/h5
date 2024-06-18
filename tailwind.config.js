074718f508d891f3377988f2ba81ac88b7e72d9b
horacioBerrueta <166517284+horacioBerrueta@users.noreply.github.com>
2024-06-18 17:58:50 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["index.html", "./src/**/*.{jsx,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "brand-100": "#0E3C54"
+      }
+    },
+  },
+  plugins: [],
+}
+
